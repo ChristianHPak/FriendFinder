c944246f30fe9d848785841455c9b1c039c95325
Christian Pak <iamchristianpak@gmail.com>
2019-07-10 15:50:15 -0600

    final

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,27 +1,27 @@
 var friends = [
 	{
-		name: "Charlie",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/0/0a/Charlie_%289%29.jpg",
+		name: "Dwight",
+		photo: "http://www.comedycentral.co.uk/sites/default/files/styles/image-w-760-h-428-scale-crop/public/cc_uk/arc/2018/10/26/3e1b32d0-8561-4883-b086-8e1506184853.jpg?itok=iuPbmQge",
 		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
 	},
 	{
 		name: "Sweet Dee",
-		photo: "https://vignette2.wikia.nocookie.net/itsalwayssunny/images/f/f3/Square-Dee-Cheesy.jpg",
+		photo: "https://i.redd.it/c6zghxivwf921.jpg",
 		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
 	},
 	{
 		name: "Dennis",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/1/13/Dennis_%281%29.jpg",
+		photo: "https://i.pinimg.com/originals/a1/ef/01/a1ef01e34a33dd506cb3905e1cdc869c.jpg",
 		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
 	},
 	{
 		name: "Mac",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/5/5f/Mac_-_Season_8.jpg",
+		photo: "https://i.redd.it/5l1gthcorw001.jpg",
 		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
 	},
 	{
 		name: "Frank",
-		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/f/fe/Season_7_-_Frank.jpg",
+		photo: "https://littlelesslove.files.wordpress.com/2015/07/vlcsnap-2015-07-21-11h46m56s90.png",
 		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
 	}
 ];
